b55aef2b944733d4d912e9294f05a50a7b4d6aea
Amanullah Hussain <amanhussi67@gmail.com>
2025-12-06 20:13:09 +0530

    update app.py for deployment

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from config import Config
+from dotenv import load_dotenv
+load_dotenv()
 import os
 import sys
 
