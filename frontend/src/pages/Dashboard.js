9fd258c3243993b3d949678f88a2210931ba26f8
Amanullah Hussain <amanhussi67@gmail.com>
2026-01-26 10:57:51 +0530

    summaryaaasa

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -337,8 +337,7 @@ function Dashboard({ darkMode, setDarkMode }) {
                 />
                 <div className="option-icon ai-icon">
                   <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M13 7H7v6h6V7z"/>
-                    <path fillRule="evenodd" d="M7 2a1 1 0 012 0v1h2V2a1 1 0 112 0v1h2a2 2 0 012 2v2h1a1 1 0 110 2h-1v2h1a1 1 0 110 2h-1v2a2 2 0 01-2 2h-2v1a1 1 0 11-2 0v-1H9v1a1 1 0 11-2 0v-1H5a2 2 0 01-2-2v-2H2a1 1 0 110-2h1V9H2a1 1 0 010-2h1V5a2 2 0 012-2h2V2zM5 5h10v10H5V5z" clipRule="evenodd"/>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                   </svg>
                 </div>
                 <div className="option-content">
@@ -465,8 +464,8 @@ function Dashboard({ darkMode, setDarkMode }) {
             
             <div className="action-card-modern" onClick={() => navigate('/history')}>
               <div className="action-icon-wrapper history-gradient">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                <svg width="24" height="24" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd"/>
                 </svg>
               </div>
               <div className="action-card-content">
